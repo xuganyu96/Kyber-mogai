9dc2358d31646d39eac5b7efa799f661c1492409
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2024-09-07 13:55:02 -0400

    Experimented with using time.h API

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdint.h>
+
+uint64_t to_microseconds(struct timespec *timer) {
+  return (timer->tv_sec * 1000000) + (timer->tv_nsec / 1000);
+}
 
 int main(void) {
-  printf("Hello, world!\n");
+  struct timespec timer;
+
+  if (0 != clock_gettime(CLOCK_MONOTONIC, &timer)) {
+    fprintf(stderr, "Monotonic clock returned error\n");
+  } else {
+    printf("Seconds: %ld, nanosecond: %ld\n", timer.tv_sec, timer.tv_nsec);
+  printf("microseconds: %llu\n", to_microseconds(&timer));
+  }
 
   return 0;
 }
