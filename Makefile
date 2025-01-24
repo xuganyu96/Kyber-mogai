60c97ca2a843383e7d5a069bd8553f65e93f9a58
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2025-01-24 11:11:49 -0500

    don't know how to use compiled AVX stastic libraries

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CFLAGS += -O3
 LDFLAGS += -lcrypto
 
-target/main: main.c
-	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $<
+target/main: main.c static-mceliece/libmceliece348864favx.a
+	$(CC) $(CFLAGS) $(LDFLAGS) -DMCELIECE_N=3488 -o $@ $^
--- main.c
+++ main.c
@@ -9,6 +9,8 @@
 #include <stdlib.h>
 
 #include "easy-mceliece.h"
+#include "easy-mceliece/avx/mceliece348864/decrypt.h"
+#include "easy-mceliece/avx/mceliece348864/encrypt.h"
 
 static void println_hexstr(uint8_t *bytes, size_t arrlen) {
   printf("0x");
@@ -21,12 +23,29 @@ static void println_hexstr(uint8_t *bytes, size_t arrlen) {
 int main(void) {
   uint8_t pk[CRYPTO_PUBLICKEYBYTES];
   uint8_t sk[CRYPTO_SECRETKEYBYTES];
-  uint8_t errvec[CRYPTO_PLAINTEXTBYTES];
+  uint8_t pt[CRYPTO_PLAINTEXTBYTES];
+  uint8_t pt_cmp[CRYPTO_PLAINTEXTBYTES];
   uint8_t ct[CRYPTO_CIPHERTEXTBYTES];
 
   crypto_kem_keypair(pk, sk);
   println_hexstr(pk, CRYPTO_PUBLICKEYBYTES);
   println_hexstr(sk, CRYPTO_SECRETKEYBYTES);
+
+  gen_e(pt);
+  println_hexstr(pt, CRYPTO_PLAINTEXTBYTES);
+
+  syndrome(ct, pk, pt);
+  println_hexstr(ct, CRYPTO_CIPHERTEXTBYTES);
+
+  cpa_decrypt(pt_cmp, sk, ct);
+  uint8_t diff = 0;
+  for (int i = 0; i < CRYPTO_PLAINTEXTBYTES; i++) {
+    diff |= pt[i] ^ pt_cmp[i];
+  }
+  if (diff) {
+    printf("CPA decryption failed\n");
+    exit(EXIT_FAILURE);
+  }
 
   exit(EXIT_SUCCESS);
 }
